765751e52e92d759d3f55ba771025cf8dd60bb95
Saad Mehmoon <47217376+saadmehmoon@users.noreply.github.com>
2023-03-22 09:20:09 +0500

    LAD-63 Added encrypted storage and added back buttons

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useNavigation} from '@react-navigation/native';
 
 import Login from '../screens/Login';
 import Signup from '../screens/Signup';
 import Otp from '../screens/Otp';
+import { Image, Text, TouchableOpacity } from 'react-native';
 
 export type AuthStackParamList = {
   Login: undefined;
@@ -14,12 +16,28 @@ export type AuthStackParamList = {
 const AuthStack = createNativeStackNavigator<AuthStackParamList>();
 
 const Auth = () => {
+  const navigation = useNavigation();
   return (
     <AuthStack.Navigator
-      screenOptions={{headerShown: false}}
+      screenOptions={{
+        header: () => (
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Image
+              style={{
+                position: 'absolute',
+                width: 24,
+                height: 24,
+                marginLeft: 20,
+                marginTop: 20,
+              }}
+              source={require('../assets/images/back-arrow.png')} 
+            />
+          </TouchableOpacity>
+        ),
+      }}
       initialRouteName="Login">
-      <AuthStack.Screen name="Login" component={Login} />
-      <AuthStack.Screen name="Signup" component={Signup} />
+      <AuthStack.Screen name="Login" component={Login} options={{headerShown: false}} />
+      <AuthStack.Screen name="Signup" component={Signup} options={{headerShown: false}} />
       <AuthStack.Screen name="Otp" component={Otp} />
     </AuthStack.Navigator>
   );
