572efdec9340d31ee9dc9772625a949a6bcfb0d4
Saad Mehmoon <47217376+saadmehmoon@users.noreply.github.com>
2023-03-31 23:17:42 +0500

    LAD-9 Added API call for registration
    the flow for registration is complete.
    Phone verification is on pending now

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -1,8 +1,9 @@
-import {ScrollView, StyleSheet, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
 import {useTheme} from '@react-navigation/native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
+import {register} from '../api/Auth';
 import {AuthStackParamList} from '../navigation/AuthStack';
 import Logo from '../components/Logo';
 import HrText from '../components/HrText';
@@ -13,6 +14,31 @@ type SignupProps = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
 
 const Signup: React.FC<SignupProps> = ({navigation}) => {
   const {colors} = useTheme();
+  const [rollNumber, setRollNumber] = React.useState('');
+  const [phoneNumber, setPhoneNumber] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [fullName, setFullName] = React.useState('');
+  const [gender, setGender] = React.useState('');
+
+  const handleSubmit = async () => {
+    try {
+      const res = await register(
+        Number(rollNumber),
+        password,
+        fullName,
+        gender,
+        phoneNumber,
+      );
+      console.log(res);
+      navigation.navigate('Otp', {
+        rollNumber: Number(rollNumber),
+        path: 'signup', // signup to otp -> gives finish btn
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.logoContainer}>
@@ -22,20 +48,35 @@ const Signup: React.FC<SignupProps> = ({navigation}) => {
         <HrText hrColor={colors.text} textStyle={{color: colors.text}}>
           Sign Up
         </HrText>
-        <AppTextInput placeholder="Full Name" />
-        <AppTextInput placeholder="Roll Number" keyboardType='numeric' />
-        <AppTextInput secureTextEntry placeholder="Password" />
-        <AppTextInput placeholder="Gender" />
-        <AppTextInput placeholder="Phone Number" keyboardType='numeric' />
-        <Button
-          primary
-          onPress={() =>
-            navigation.navigate('Otp', {
-              rollNumber: '24100043',
-              phoneNumber: '+92 303 22 33 203', // TODO: replace it with user input
-              path: 'signup', // signup to otp -> gives finish btn
-            })
-          }>
+        <AppTextInput
+          placeholder="Full Name"
+          value={fullName}
+          onChangeText={setFullName}
+        />
+        <AppTextInput
+          placeholder="Roll Number"
+          keyboardType="numeric"
+          value={rollNumber}
+          onChangeText={setRollNumber}
+        />
+        <AppTextInput
+          secureTextEntry
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+        />
+        <AppTextInput
+          placeholder="Gender"
+          value={gender}
+          onChangeText={setGender}
+        />
+        <AppTextInput
+          placeholder="Phone Number"
+          keyboardType="numeric"
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+        />
+        <Button primary onPress={handleSubmit}>
           Continue
         </Button>
         <HrText hrColor={colors.text} textStyle={{color: colors.text}}>
