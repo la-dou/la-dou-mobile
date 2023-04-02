a601e8d5a2b5d099569c28bfd91cd3e4417d3397
rafayy769 <rafayy769@gmail.com>
2023-04-03 01:56:52 +0500

    Well added some state etc ahhh

--- src/screens/PostJobs.tsx
+++ src/screens/PostJobs.tsx
@@ -17,43 +17,83 @@ const PostJobs = () => {
   const [oldPassword, setOldPassword] = React.useState('');
   const [newPassword, setNewPassword] = React.useState('');
 
+  const [pickup, setPickup] = React.useState('');
+  const [dropoff, setDropoff] = React.useState('');
+  const [deliveryPrice, setDeliveryPrice] = React.useState('');
+  const [cashToBePaid, setCashToBePaid] = React.useState('');
+  const [deliveryNote, setDeliveryNote] = React.useState('');
+
   const handleSubmit = async () => {
-    //add validation for empty fields, password length, etc
-    if (!oldPassword) {
-      Alert.alert('Please enter your old password');
+    //add validation for empty fields
+    if (!pickup)
+    {
+      Alert.alert('Please enter your pickup location');
       return;
     }
-    if (newPassword.length < 8 && newPassword.length > 0) {
-      Alert.alert('Password must be at least 8 characters');
+    if (!dropoff)
+    {
+      Alert.alert('Please enter your dropoff location');
       return;
     }
-    if (phoneNumber.length !== 11) {
-      Alert.alert('Please enter a valid phone number');
+    if (!deliveryPrice)
+    {
+      Alert.alert('Please enter your delivery price');
       return;
     }
-    if (phoneNumber !== userDetails.phone_number) {
-      try {
-        const res = await updatePhone(oldPassword, phoneNumber);
-        console.log(res);
-        setUserDetails({...userDetails, phone_number: phoneNumber});
-        Alert.alert('Phone number updated successfully!');
-      } catch (err) {
-        console.log(err);
-        Alert.alert('Error updating phone number!');
-      }
-    }
-    if (newPassword !== '') {
-      try {
-        const res = await updatePassword(oldPassword, newPassword);
-        console.log(res);
-        Alert.alert('Password updated successfully!');
-      } catch (err) {
-        console.log(err);
-        Alert.alert('Error updating password!');
-      }
+    if (!cashToBePaid)
+    {
+      Alert.alert('Please enter your cash to be paid');
+      return;
     }
 
-    console.log('submit');
+    // add validation for delivery price and cash to be paid. convert to number and check if it is a number
+    if (isNaN(Number(deliveryPrice)))
+    {
+      Alert.alert('Please enter a valid delivery price');
+      return;
+    }
+
+    if (isNaN(Number(cashToBePaid)))
+    {
+      Alert.alert('Please enter a valid amount to pay.');
+      return;
+    }
+
+    // if (!oldPassword) {
+    //   Alert.alert('Please enter your old password');
+    //   return;
+    // }
+    // if (newPassword.length < 8 && newPassword.length > 0) {
+    //   Alert.alert('Password must be at least 8 characters');
+    //   return;
+    // }
+    // if (phoneNumber.length !== 11) {
+    //   Alert.alert('Please enter a valid phone number');
+    //   return;
+    // }
+    // if (phoneNumber !== userDetails.phone_number) {
+    //   try {
+    //     const res = await updatePhone(oldPassword, phoneNumber);
+    //     console.log(res);
+    //     setUserDetails({...userDetails, phone_number: phoneNumber});
+    //     Alert.alert('Phone number updated successfully!');
+    //   } catch (err) {
+    //     console.log(err);
+    //     Alert.alert('Error updating phone number!');
+    //   }
+    // }
+    // if (newPassword !== '') {
+    //   try {
+    //     const res = await updatePassword(oldPassword, newPassword);
+    //     console.log(res);
+    //     Alert.alert('Password updated successfully!');
+    //   } catch (err) {
+    //     console.log(err);
+    //     Alert.alert('Error updating password!');
+    //   }
+    // }
+
+    console.log('Post Order Pressed');
   };
 
   return (
@@ -63,32 +103,32 @@ const PostJobs = () => {
       </HrText>
       <AppTextInput
         placeholder="Pickup"
-        value={newPassword}
-        onChangeText={setNewPassword}
+        value={pickup}
+        onChangeText={setPickup}
       />
       <AppTextInput
         placeholder="Dropoff"
-        value={newPassword}
-        onChangeText={setNewPassword}
+        value={dropoff}
+        onChangeText={setDropoff}
       />
       <AppTextInput
         placeholder="Delivery Price"
-        value={phoneNumber}
-        onChangeText={setPhoneNumber}
+        value={deliveryPrice}
+        onChangeText={setDeliveryPrice}
         keyboardType="phone-pad"
         // maxLength={11}
       />
       <AppTextInput
         placeholder="Cash to be paid"
-        value={phoneNumber}
-        onChangeText={setPhoneNumber}
+        value={cashToBePaid}
+        onChangeText={setCashToBePaid}
         keyboardType="phone-pad"
         // maxLength={11}
       />
       <AppTextInput
         placeholder="Additional Notes (if any)"
-        value={newPassword}
-        onChangeText={setNewPassword}
+        value={deliveryNote}
+        onChangeText={setDeliveryNote}
         containerStyle={styles.lastTextBox}
       />
 
