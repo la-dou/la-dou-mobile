c9b5ce2bfa4754eca65f42fa1b11c887793c1104
rafayy769 <rafayy769@gmail.com>
2023-04-03 05:35:53 +0500

    Successfully able to post jobs

--- src/screens/PostJobs.tsx
+++ src/screens/PostJobs.tsx
@@ -1,23 +1,13 @@
 import {StyleSheet, Alert, View} from 'react-native';
 import React from 'react';
 import {useTheme} from '@react-navigation/native';
-import {useRecoilState} from 'recoil';
 import HrText from '../components/HrText';
 import AppButton from '../components/Button';
 import AppTextInput from '../components/AppTextInput';
-import {userDetails as userDetailsAtom} from '../atoms';
-import {updatePhone, updatePassword} from '../api/User';
 import { postJob } from '../api/PostJob';
 
 const PostJobs = () => {
   const {colors} = useTheme();
-  // const [userDetails, setUserDetails] = useRecoilState(userDetailsAtom);
-  // const [phoneNumber, setPhoneNumber] = React.useState(
-  //   userDetails.phone_number,
-  // );
-  // const [oldPassword, setOldPassword] = React.useState('');
-  // const [newPassword, setNewPassword] = React.useState('');
-
   const [pickup, setPickup] = React.useState('');
   const [dropoff, setDropoff] = React.useState('');
   const [deliveryPrice, setDeliveryPrice] = React.useState('');
@@ -48,55 +38,24 @@ const PostJobs = () => {
     }
 
     // add validation for delivery price and cash to be paid. convert to number and check if it is a number
-    if (isNaN(Number(deliveryPrice)))
+    if (isNaN(Number(deliveryPrice)) || Number(deliveryPrice) < 0)
     {
       Alert.alert('Please enter a valid delivery price');
       return;
     }
 
-    if (isNaN(Number(cashToBePaid)))
+    if (isNaN(Number(cashToBePaid)) || Number(cashToBePaid) < 0)
     {
       Alert.alert('Please enter a valid amount to pay.');
       return;
     }
 
-    // if (!oldPassword) {
-    //   Alert.alert('Please enter your old password');
-    //   return;
-    // }
-    // if (newPassword.length < 8 && newPassword.length > 0) {
-    //   Alert.alert('Password must be at least 8 characters');
-    //   return;
-    // }
-    // if (phoneNumber.length !== 11) {
-    //   Alert.alert('Please enter a valid phone number');
-    //   return;
-    // }
-    // if (phoneNumber !== userDetails.phone_number) {
-    //   try {
-    //     const res = await updatePhone(oldPassword, phoneNumber);
-    //     console.log(res);
-    //     setUserDetails({...userDetails, phone_number: phoneNumber});
-    //     Alert.alert('Phone number updated successfully!');
-    //   } catch (err) {
-    //     console.log(err);
-    //     Alert.alert('Error updating phone number!');
-    //   }
-    // }
-    // if (newPassword !== '') {
-    //   try {
-    //     const res = await updatePassword(oldPassword, newPassword);
-    //     console.log(res);
-    //     Alert.alert('Password updated successfully!');
-    //   } catch (err) {
-    //     console.log(err);
-    //     Alert.alert('Error updating password!');
-    //   }
-    // }
+    if (Number(cashToBePaid) < Number(deliveryPrice))
+    {
+      Alert.alert('Cash to be paid cannot be less than delivery price');
+      return;
+    }
 
-    console.log('Post Order Pressed');
-    return;
-    
     try 
     {
       const res = await postJob(pickup, dropoff, deliveryPrice, cashToBePaid, deliveryNote);
@@ -131,14 +90,14 @@ const PostJobs = () => {
         value={deliveryPrice}
         onChangeText={setDeliveryPrice}
         keyboardType="phone-pad"
-        // maxLength={11}
+        maxLength={6}
       />
       <AppTextInput
         placeholder="Cash to be paid"
         value={cashToBePaid}
         onChangeText={setCashToBePaid}
         keyboardType="phone-pad"
-        // maxLength={11}
+        maxLength={6}
       />
       <AppTextInput
         placeholder="Additional Notes (if any)"
