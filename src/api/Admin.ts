f3daf34758b35d2401efefee4b5961cdc1df349e
ps-yduck <88939360+ps-yduck@users.noreply.github.com>
2023-04-06 21:19:06 +0500

    search screen updated

--- src/api/Admin.ts
+++ src/api/Admin.ts
@@ -1,14 +1,21 @@
 import axios from 'axios';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 const API_URL = 'http://10.0.2.2:8000';
 
 export const adminGetUsers = async (search: string) => {
+  const token = await EncryptedStorage.getItem('token');
   const config = {
     method: 'get',
     url: `${API_URL}/admin/search?search=${search}`,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
   };
+
   const response = await axios(config);
-  console.log(response.data);
-  console.log(typeof response.data);
+  // console.log(response.data);
+  // console.log(typeof response.data);
   return response.data;
 };
--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -11,90 +11,14 @@ import PrimaryTheme from '../theme/Primary';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {MainStackParamList} from '../navigation/MainStack';
 import {adminGetUsers} from '../api/Admin';
-
-type UserCardProps = {
-  name: string;
-  rating_as_customer: string;
-  rating_as_driver: string;
-  roll_no: string;
-};
-const UserCard = (data: UserCardProps) => {
-  console.log(typeof data.rating_as_customer);
-  return (
-    <View style={styles.cardContainer}>
-      <View style={styles.textBar}>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingLeft: 20,
-          }}>
-          <Text style={styles.textLabel}>Name: </Text>
-          <Text style={styles.textStyle}>{data.name}</Text>
-        </View>
-        {/* add star in driver rating */}
-
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingRight: 20,
-          }}>
-          <Text style={styles.textLabel}>Customer: </Text>
-          <Text style={styles.textStyle}>{data.rating_as_customer}</Text>
-          {data.rating_as_customer === 'N/A' ? null : (
-            // <Image
-            //   source={require('../assets/images/star-filled.png')}
-            //   style={{width: 15, height: 15, marginLeft: 5}}
-            // />
-            <Text>⭐️</Text>
-          )}
-        </View>
-
-        {/* star */}
-      </View>
-      <View style={styles.textBar}>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingLeft: 20,
-          }}>
-          <Text style={styles.textLabel}>Roll No: </Text>
-          <Text style={[styles.textStyle, {paddingLeft: 4}]}>
-            {data.roll_no}
-          </Text>
-        </View>
-        {/* add star in driver rating */}
-
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingRight: 20,
-          }}>
-          <Text style={styles.textLabel}>Driver: </Text>
-          <Text style={styles.textStyle}>{data.rating_as_driver}</Text>
-          {data.rating_as_driver === 'N/A' ? null : (
-            // <Image
-            //   source={require('../assets/images/star-filled.png')}
-            //   style={{width: 15, height: 15, marginLeft: 5}}
-            // />
-            <Text style={{marginLeft: 5, alignSelf: 'flex-start'}}>⭐️</Text>
-          )}
-        </View>
-
-        {/* star */}
-      </View>
-    </View>
-  );
-};
+import InfoCard from '../components/InfoCard';
 
 type SearchProps = NativeStackScreenProps<MainStackParamList, 'Search'>;
 const Search: React.FC<SearchProps> = () => {
   const [searchVal, setSearchVal] = React.useState('');
   const [userList, setUserList] = React.useState<any>([]);
   const onChangeSetSearchVal = async (val: string) => {
+    //console.log(val);
     setSearchVal(val);
     const res = await adminGetUsers(val);
     setUserList(res);
@@ -118,9 +42,42 @@ const Search: React.FC<SearchProps> = () => {
       </View>
       {/* scrolll */}
 
-      {userList.map((user: any) => (
-        <UserCard {...user} />
-      ))}
+      <ScrollView showsVerticalScrollIndicator={false} style={{marginTop: 20}}>
+        {userList.map((item: any, index: any) => {
+          return (
+            <InfoCard
+              //add onPress for eac card to navigate to user profile
+              // each item in userList have UserSearch Model (see backend for model)
+              // at the moment total amount is kept zero as db does not have amount info
+              key={index}
+              data={[
+                {
+                  title: 'Name: ',
+                  data: item.name,
+                },
+                {
+                  title: 'Customer: ',
+                  data:
+                    item.rating_as_customer === 'N/A'
+                      ? 'N/A'
+                      : item.rating_as_customer + '⭐️',
+                },
+                {
+                  title: 'Roll No: ',
+                  data: item.roll_no,
+                },
+                {
+                  title: 'Driver: ',
+                  data:
+                    item.rating_as_driver === 'N/A'
+                      ? 'N/A'
+                      : item.rating_as_driver + '⭐️',
+                },
+              ]}
+            />
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
@@ -129,12 +86,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
 
-    alignItems: 'center',
     marginTop: 90,
+    width: '100%',
+    padding: 20,
   },
   //card stylying
   searchContainer: {
-    width: '94%',
+    width: '100%',
     height: 50,
     backgroundColor: PrimaryTheme.colors.card,
     borderRadius: 10,
@@ -157,49 +115,6 @@ const styles = StyleSheet.create({
     color: PrimaryTheme.colors.primary,
     fontFamily: 'Montserrat-Regular',
     fontSize: 16,
-  },
-  //card stylying
-  cardContainer: {
-    //scroll
-
-    //for each card
-    backgroundColor: '#262626',
-
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#CB4B47',
-    width: '94%',
-    height: 100,
-    marginTop: 30,
-
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  textBar: {
-    //for each row
-    width: '100%',
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  textStyle: {
-    //for content
-    fontSize: 12,
-    fontFamily: 'Montserrat-Medium',
-    height: 15,
-    fontWeight: '600',
-    color: PrimaryTheme.colors.primary,
-    lineHeight: 14,
-    paddingLeft: 10,
-  },
-  textLabel: {
-    //for label
-    fontSize: 12,
-    fontFamily: 'Montserrat-Regular',
-    height: 15,
-    fontWeight: '400',
-    color: PrimaryTheme.colors.primary,
-    lineHeight: 14,
   },
 });
 
