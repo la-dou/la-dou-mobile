4e75c15189740076b4de4dae809fff3cdb5ea7bd
ps-yduck <88939360+ps-yduck@users.noreply.github.com>
2023-04-09 18:14:23 +0500

    waiting screen

--- src/screens/WaitScreen.tsx
+++ src/screens/WaitScreen.tsx
@@ -1,16 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+import {MainStackParamList} from '../navigation/MainStack';
+import PrimaryTheme from '../theme/Primary';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
-type Props = {}
-
-const WaitScreen = (props: Props) => {
+type WaitScreenProps = NativeStackScreenProps<MainStackParamList, 'WaitScreen'>;
+const WaitScreen: React.FC<WaitScreenProps> = ({navigation}) => {
   return (
-    <View>
-      <Text>WaitScreen</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View
+        style={{
+          width: 300,
+          height: 180,
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <Image
+          source={require('../assets/images/logo-waiting.png')}
+          style={{width: 100, height: 100}}
+        />
+
+        <View style={{alignItems: 'center'}}>
+          <Text
+            style={{
+              fontSize: 20,
+              fontFamily: 'Montserrat-bold',
+              fontWeight: '600',
+              color: PrimaryTheme.colors.primary,
+              lineHeight: 24,
+            }}>
+            Waiting for the Customer
+          </Text>
+          <Text
+            style={{
+              fontSize: 20,
+              fontFamily: 'Montserrat-bold',
+              fontWeight: '600',
+              color: PrimaryTheme.colors.primary,
+              lineHeight: 24,
+            }}>
+            to make a decision...
+          </Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default WaitScreen
-
-const styles = StyleSheet.create({})
+export default WaitScreen;
